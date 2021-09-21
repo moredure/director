d472f5ee4f1da5f9444271f5e1b16bc85acc6f24
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-09-21 22:50:15 +0300

    Update direct_reader.go

--- direct_reader.go
+++ direct_reader.go
@@ -34,3 +34,39 @@ func NewDirectReader(r io.Reader, buf []byte) io.Reader {
 		buf: buf,
 	}
 }
+
+/*
+type directReader struct {
+	rd io.Reader
+	buf []byte
+	r, w int
+}
+
+func (r *directReader) Read(b []byte) (n int, err error) {
+	if r.r > 0 {
+		n = copy(b, r.buf[r.r:r.w])
+		if n < (r.w - r.r) {
+			r.r += n
+			return
+		} else {
+			r.r = 0
+		}
+	}
+	rx, err := r.rd.Read(r.buf)
+	if err != nil {
+		return n + rx, err
+	}
+	nx := copy(b[n:], r.buf[:rx])
+	if (rx - nx) > 0 {
+		r.r, r.w = nx, rx
+	}
+	return nx + n, nil
+}
+
+func NewDirectReader(r io.Reader, buf []byte) io.Reader {
+	return &directReader{
+		r: r,
+		buf: buf,
+	}
+}
+*/
