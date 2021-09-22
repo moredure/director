0a9c53f30f80530de03e56b07108dde470000743
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-09-22 14:10:39 +0300

    Update direct_reader.go

--- direct_reader.go
+++ direct_reader.go
@@ -6,55 +6,55 @@ import (
 )
 
 // adaptation of bufio.Reader for directio
-type directReader struct {
-	rd io.Reader
-	buf []byte
+type DirectReader struct {
+	rd   io.Reader
+	buf  []byte
 	r, w int
-	err error
+	err  error
 }
 
 var errNegativeRead = errors.New("err negative read")
 
-func (b *directReader) Read(p []byte) (n int, err error) {
+func (d *DirectReader) Read(p []byte) (n int, err error) {
 	n = len(p)
 	if n == 0 {
-		if b.Buffered() > 0 {
+		if d.Buffered() > 0 {
 			return 0, nil
 		}
-		return 0, b.readErr()
+		return 0, d.readErr()
 	}
-	if b.r == b.w {
-		if b.err != nil {
-			return 0, b.readErr()
+	if d.r == d.w {
+		if d.err != nil {
+			return 0, d.readErr()
 		}
-		b.r = 0
-		b.w = 0
-		n, b.err = b.rd.Read(b.buf)
+		d.r = 0
+		d.w = 0
+		n, d.err = d.rd.Read(d.buf)
 		if n < 0 {
 			panic(errNegativeRead)
 		}
 		if n == 0 {
-			return 0, b.readErr()
+			return 0, d.readErr()
 		}
-		b.w += n
+		d.w += n
 	}
 
-	n = copy(p, b.buf[b.r:b.w])
-	b.r += n
+	n = copy(p, d.buf[d.r:d.w])
+	d.r += n
 	return n, nil
 }
 
-func (b *directReader) Buffered() int { return b.w - b.r }
+func (d *DirectReader) Buffered() int { return d.w - d.r }
 
-func (b *directReader) readErr() error {
-	err := b.err
-	b.err = nil
+func (d *DirectReader) readErr() error {
+	err := d.err
+	d.err = nil
 	return err
 }
 
-func NewDirectReader(r io.Reader, buf []byte) io.Reader {
-	return &directReader{
-		rd: r,
+func NewDirectReader(r io.Reader, buf []byte) *DirectReader {
+	return &DirectReader{
+		rd:  r,
 		buf: buf,
 	}
 }
