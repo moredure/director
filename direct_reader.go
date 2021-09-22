f5a831834315e78e95b2769c2b802102ec50cf26
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-09-22 14:08:15 +0300

    Update direct_reader.go

--- direct_reader.go
+++ direct_reader.go
@@ -1,25 +1,55 @@
 package director
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
+// adaptation of bufio.Reader for directio
 type directReader struct {
 	rd io.Reader
 	buf []byte
 	r, w int
+	err error
 }
 
-func (d *directReader) Read(b []byte) (n int, err error) {
-	if d.r == d.w {
-		d.r, err = d.rd.Read(d.buf)
-		if err != nil {
-			return 0, err
+var errNegativeRead = errors.New("err negative read")
+
+func (b *directReader) Read(p []byte) (n int, err error) {
+	n = len(p)
+	if n == 0 {
+		if b.Buffered() > 0 {
+			return 0, nil
 		}
-		d.w = copy(b, d.buf[:d.r])
-		return d.w, nil
+		return 0, b.readErr()
 	}
-	n = copy(b, d.buf[d.w:d.r])
-	d.w += n
+	if b.r == b.w {
+		if b.err != nil {
+			return 0, b.readErr()
+		}
+		b.r = 0
+		b.w = 0
+		n, b.err = b.rd.Read(b.buf)
+		if n < 0 {
+			panic(errNegativeRead)
+		}
+		if n == 0 {
+			return 0, b.readErr()
+		}
+		b.w += n
+	}
+
+	n = copy(p, b.buf[b.r:b.w])
+	b.r += n
 	return n, nil
+}
+
+func (b *directReader) Buffered() int { return b.w - b.r }
+
+func (b *directReader) readErr() error {
+	err := b.err
+	b.err = nil
+	return err
 }
 
 func NewDirectReader(r io.Reader, buf []byte) io.Reader {
