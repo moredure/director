3bc808abd6856809780b05458a27ac91510695a7
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-09-22 13:37:50 +0300

    Update direct_reader.go

--- direct_reader.go
+++ direct_reader.go
@@ -3,40 +3,6 @@ package director
 import "io"
 
 type directReader struct {
-	r io.Reader
-	buf, rest []byte
-}
-
-func (d *directReader) Read(b []byte) (n int, err error) {
-	if d.rest != nil {
-		n = copy(b, d.rest)
-		if n < len(d.rest) {
-			d.rest = d.rest[n:]
-			return
-		} else {
-			d.rest = nil
-		}
-	}
-	r, err := d.r.Read(d.buf)
-	if err != nil {
-		return n + r, err
-	}
-	w := copy(b[n:], d.buf[:r])
-	if (r - w) > 0 {
-		d.rest = d.buf[w:r]
-	}
-	return w + n, nil
-}
-
-func NewDirectReader(r io.Reader, buf []byte) io.Reader {
-	return &directReader{
-		r: r,
-		buf: buf,
-	}
-}
-
-/*
-type directReader struct {
 	rd io.Reader
 	buf []byte
 	r, w int
@@ -46,21 +12,19 @@ func (d *directReader) Read(b []byte) (n int, err error) {
 	if d.r == d.w {
 		d.r, err = d.rd.Read(d.buf)
 		if err != nil {
-			return
+			return 0, err
 		}
 		d.w = copy(b, d.buf[:d.r])
 		return d.w, nil
-
 	}
 	n = copy(b, d.buf[d.w:d.r])
 	d.w += n
-	return
+	return n, nil
 }
 
 func NewDirectReader(r io.Reader, buf []byte) io.Reader {
 	return &directReader{
-		r: r,
+		rd: r,
 		buf: buf,
 	}
 }
-*/
